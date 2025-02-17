fe8b61b20309c661b5431d6c8edb5e75cbab9967
suehee <saltychips.hwang@gmail.com>
2025-02-17 14:12:57 +0900

    푸터 svg로

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import FOOTER_IMAGE from '../assets/images/footer_pc.svg';
+
+//푸터 스타일
+const Nav = styled.nav`
+  top: 0;
+  width: 100%;
+  height: auto;
+  background-color: #EBEBF0; /* gray50 */
+  display: flex;
+  align-items: center;
+  justify-content : center;
+  z-index: 800;
+`;
 
 const Footer = () => {
   return (
-    <div>
-      <img src="src/assets/images/footer_pc.png" alt="footer"/>
-    </div>
-  )
-}
+        <Nav>
+          <img id="THIS_IS_FOOTER" src={FOOTER_IMAGE} width="800" height="auto" />
+        </Nav>
+  );
+};
 
 export default Footer
