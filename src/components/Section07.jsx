fb56d4fa099d57d5ec1b77d3ea8606134a4f7e3c
yerin <jyr384848@gmail.com>
2025-02-19 17:31:28 +0900

    섹션 7 지성씨 추가

--- src/components/Section07.jsx
+++ src/components/Section07.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import Headline from "./Headline";
+import videoSrc from "../assets/images/section07.mp4"; // 비디오 파일 가져오기
 
 const SectionContainer = styled.section`
   width: 100%;
@@ -11,27 +12,39 @@ const SectionContainer = styled.section`
   justify-content: center;
   text-align: center;
   color: white;
-  background-image: url("/src/assets/images/S07_background_img.png");
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
+  position: relative; // 비디오를 배경처럼 사용하기 위해 상대 위치 지정
+  overflow: hidden;
+`;
+
+const BackgroundVideo = styled.video`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  transform: translate(-50%, -50%);
+  z-index: -1; // 텍스트 위에 오지 않도록 뒤로 보내기
 `;
 
 const ContentWrapper = styled.div`
   max-width: 900px;
   text-align: center;
+  position: relative; // 텍스트가 비디오 위에 정상적으로 표시되도록
+  z-index: 1;
 `;
 
 const Section07 = () => {
   return (
-    <SectionContainer> 
+    <SectionContainer>
+      <BackgroundVideo src={videoSrc} autoPlay loop muted playsInline />
       <ContentWrapper>
         <Headline
-        title="국경과 언어를 모두 넘어 우리의 궁금증을 실시간으로 나누는 곳!"
-        title_width="650px"
-        description="궁금한 거 있으면 물어도 보고, 핫이슈로 토크배틀도 하고!"
-        description_width="500px"
-      />
+          title="국경과 언어를 모두 넘어 우리의 궁금증을 실시간으로 나누는 곳!"
+          title_width="650px"
+          description="궁금한 거 있으면 물어도 보고, 핫이슈로 토크배틀도 하고!"
+          description_width="500px"
+        />
       </ContentWrapper>
     </SectionContainer>
   );
